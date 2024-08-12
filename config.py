7834140d6a11c868ea75cb9eaa04db99273e28a1
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-08-12 08:26:44 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+#ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
+from os import environ
+import os
+
+API_ID = int(os.environ.get("API_ID", "10811400"))
+API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7412278588:AAHmk19iP3uK79OglBISjicbl70TD6i9wEc")
+ADMIN = int(os.environ.get("ADMIN", "6469754522")) 
+FSUB_CHANNEL = os.environ.get("FSUB_CHANNEL", "Sunrises24BotUpdates")
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://INFINITYRENAME24BOT:INFINITYRENAME24BOT@cluster0.colorb9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
+WEBHOOK = bool(os.environ.get("WEBHOOK", True))
+PORT = int(os.environ.get("PORT", "8081"))
+#ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
